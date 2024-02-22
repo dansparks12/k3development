adb7c1fb9cea5a384da1302f0f0078f4c6c1f81a
Dan Sparks <danielsparks@Dans-iMac.local>
2024-02-22 16:40:26 +0000

    updates

--- classes/Marketing.php
+++ classes/Marketing.php
@@ -10,9 +10,69 @@ class Marketing {
         $this->_db = DB::getInstance();
     }
 
+    public function update($fields = array(), $id = null) {
+        if(!$id) {
+            $id = $this->data()->marketing_id;
+        }
+     
+        if(!$this->_db->update('marketing', 'marketing_id', $id, $fields)) {
+            throw new Exception('There was a problem updating');
+        }
+        
+    }
 
+    public function create($fields) {
+        if (!$this->_db->insert('marketing', $fields)) {
+            throw new Exception('There was a problem inserting into the database');
+        }
+    }
 
+    public function find($user = null)
+    {
+      if($user) {
+        $field = (is_numeric($user)) ? 'id' : 'username';
+        $data = $this->_db->get('marketing', array($field, '=', $user));
+        if($data->count()) {
+          $this->_data = $data->first();
+          return true;
+        }
+      }
+      return false;
+    }
+    public function findID($userid = null) {
+      if($userid) {
+        $field = (is_numeric($userid)) ? 'marketing_id' : 'marketing_name';
+        $data = $this->_db->get('marketing', array($field, '=', $userid));
+        if($data->count()) {
+          $this->_data = $data->first();
+          return true;
+        }
+      }
+      return false;
+    }
+
+    public function exists()
+    {
+      return (!empty($this->_data)) ? true : false;
+    }
+    public function data()
+  {
+    return $this->_data;
+  }
+  public function delete($row, $id) {
+    if(!$this->_db->deleteEvent('marketing', $row, $id )) {
+    throw new Exception('There was a problem deleting this event');
+    }
 }
+
+  public function totalMarketing() {
+    $sql = "SELECT * FROM marketing";
+    return $this->_db->query($sql);
+  } 
+  }
+
+
+
 
 
 
--- createevent.php
+++ createevent.php
@@ -3,6 +3,7 @@
 require "core/init.php";
 $user = new User();
 $event = new Event();
+$marketing = new Marketing();
 if (!$user->isLoggedIn()) {
     Redirect::to("index.php");
 }
@@ -153,65 +154,27 @@ if (Input::exists()) {
 												<br />
 												<h4> Please write them like this 'Sun 13 Sept 2023' and seperate with a comma. </h4>
 												<br />
-                                                <label for="eventType">eNews</label>
-												<input type="text" name="marketing1" id="marketing1" value="" placeholder="Dates" />
-                                                <br />
-												<label for="eventType">Calendars (All)</label>
-												<input type="text" name="marketing2" id="marketing2" value="" placeholder="Dates" />
-                                                <br />
-												<label for="eventType">Sunday handouts</label>
-												<input type="text" name="marketing3" id="marketing3" value="" placeholder="Dates" />
-                                                <br />
-												<label for="eventType">Handout at other event</label>
-												<input type="text" name="marketing4" id="marketing4" value="" placeholder="Dates" />
-                                                <br />
-												<label for="eventType">Church news</label>
-												<input type="text" name="marketing5" id="marketing5" value="" placeholder="Dates" />
-                                                <br />
-												<label for="eventType">Sunday slides</label>
-												<input type="text" name="marketing6" id="marketing6" value="" placeholder="Dates" />
-                                                <br />
-												<label for="eventType">Reception Screen</label>
-												<input type="text" name="marketing7" id="marketing7" value="" placeholder="Dates" />
-                                                <br />
-												<label for="eventType">Social Media</label>
-												<input type="text" name="marketing8" id="marketing8" value="" placeholder="Dates" />
-                                                <br />
-												<label for="eventType">App Feature</label>
-												<input type="text" name="marketing9" id="marketing9" value="" placeholder="Dates" />
-                                                <br />
-												<label for="eventType">Website latest</label>
-												<input type="text" name="marketing10" id="marketing10" value="" placeholder="Dates" />
-                                                <br />
-												<label for="eventType">Toilet Posters</label>
-												<input type="text" name="marketing11" id="marketing11" value="" placeholder="Dates" />
-                                                <br />
-												<label for="eventType">Kerb Banners</label>
-												<input type="text" name="marketing12" id="marketing12" value="" placeholder="Dates" />
-                                                <br />
-												<label for="eventType">Pull-up banner</label>
-												<input type="text" name="marketing13" id="marketing13" value="" placeholder="Dates" />
-                                                <br />
-												<label for="eventType">Coffee Box tables</label>
-												<input type="text" name="marketing14" id="marketing14" value="" placeholder="Dates" />
-                                                <br />
-												<label for="eventType">Key dates front</label>
-												<input type="text" name="marketing15" id="marketing15" value="" placeholder="Dates" />
-                                                <br />
-												<label for="eventType">Front door signs</label>
-												<input type="text" name="marketing16" id="marketing16" value="" placeholder="Dates" />
-                                                <br />
-												<label for="eventType">Video</label>
-												<input type="text" name="marketing17" id="marketing17" value="" placeholder="Dates" />
-                                                <br />
-												<label for="eventType">Blog</label>
-												<input type="text" name="marketing18" id="marketing18" value="" placeholder="Dates" />
-                                                <br />
-												<label for="eventType">Leadership push</label>
-												<input type="text" name="marketing19" id="marketing19" value="" placeholder="Dates" />
-                                                <br />
-												<label for="eventType">Specific Email</label>
-												<input type="text" name="marketing20" id="marketing20" value="" placeholder="Dates" />
+											
+												<select name="marketing" id="marketing">
+												<?php
+											
+												foreach($marketing->totalMarketing()->results() as $marketings) {
+														echo "<option value=" . $marketings->marketing_id . ">" . $marketings->marketing_name . "</option>";
+												}
+										
+												
+												?>
+												</select>
+												<br />
+											
+                                
+												<input type="date" name="marketing1" id="marketing1" value="" placeholder="Dates" />
+												<input type="date" name="marketing2" id="marketing1" value="" placeholder="Dates" />
+												<input type="date" name="marketing1" id="marketing1" value="" placeholder="Dates" />
+												<input type="date" name="marketing1" id="marketing1" value="" placeholder="Dates" />
+												<input type="date" name="marketing1" id="marketing1" value="" placeholder="Dates" />
+
+                                               
 									</div>
 										</div>
 										
